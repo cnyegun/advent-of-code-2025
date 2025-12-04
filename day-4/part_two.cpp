043a82e9e81e90caf075ca5138ae377fda6b18f9
Chi Nguyen <buichinguyen1105@gmail.com>
2025-12-04 09:12:59 +0200

    day 4

---
+++ day-4/part_two.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+	freopen("input", "r", stdin);
+	vector<string> M;
+
+	string line = "";
+
+	while (cin >> line) {
+		M.push_back(line);
+	}
+
+	int height = M.size();
+	int width = M[0].length();
+
+	int total = 0;
+	
+	int result = 0;
+
+	do {
+		result = 0;
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				int count = 0;
+				if (M[i][j] == '.') continue;
+				// TOP_LEFT
+				if ((i - 1) >= 0 && (j - 1) >= 0 
+					&& M[i - 1][j - 1] == '@')
+				{
+					count++;
+				}
+				// TOP
+				if ((i - 1) >= 0
+					&& M[i - 1][j] == '@')
+				{
+					count++;
+				}
+				// TOP RIGHT
+				if ((i - 1) >= 0 && (j + 1) < width
+					&& M[i - 1][j + 1] == '@')
+				{
+					count++;
+				}
+				// LEFT
+				if ((j - 1) >= 0 
+					&& M[i][j - 1] == '@')
+				{
+					count++;
+				}
+				// RIGHT
+				if ((j + 1) < width
+					&& M[i][j + 1] == '@')
+				{
+					count++;
+				}
+				// BOT LEFT
+				if ((i + 1) < height && (j - 1) >= 0
+					&& M[i + 1][j - 1] == '@')
+				{
+					count++;
+				}
+				// BOT 
+				if ((i + 1) < height
+					&& M[i + 1][j] == '@')
+				{
+					count++;
+				}
+				// BOT RIGHT
+				if ((i + 1) < height && (j + 1) < width
+					&& M[i + 1][j + 1] == '@')
+				{
+					count++;
+				}
+
+				if (count < 4) {
+					result++;
+					M[i][j] = '.';
+				}
+
+			}
+		}
+		total += result;
+
+	} while (result != 0);
+
+	cout << total  << '\n';
+
+	return 0;
+}
